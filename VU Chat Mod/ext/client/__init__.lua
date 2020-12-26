3a7f8d4edabba91968a8c13686659bb2a7da5ec9
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2020-12-26 23:27:20 +0100

    Update __init__.lua

--- VU Chat Mod/ext/client/__init__.lua
+++ VU Chat Mod/ext/client/__init__.lua
@@ -13,11 +13,30 @@ end
 CleanChat:__init()
 
 --ChatFunction [WIP]
---local RequireServer = require ('__server.__init__')
---function UserChat()
-    --if BannedWordDetected()==true then
-    --end
---end
+
+NetEvents:Subscribe('CleanChat', function(data)
+    print('Got NetEvent from server with data:')
+    print(data)
+      Hooks:Install('UI:CreateChatMessage', 100, function(hookCtx, message, channelId, playerId, recipientMask, isSenderDead)
+        if data=='Word is Banned' then
+          hookCtx:Pass('[deleted]', channelId, playerId, recipientMask, isSenderDead)
+          function reset()
+            if data=='Word is Banned' then
+              data = 'reset'
+            end
+          end
+          reset()
+        else
+          hookCtx:Pass(message, channelId, playerId, recipientMask, isSenderDead)
+        end
+      end)
+end)
+
+
+
+
+
+
 
 
 
